e44d58b6a7b854f850d341df2df1cb4754a96c5d
Marcos0221 <3091977221@qq.com>
2025-01-23 23:03:23 +0800

    :sparkles: 增加文件下载功能

--- app/src/main/java/com/example/myapplication/tool/RemoteFileManager.kt
+++ app/src/main/java/com/example/myapplication/tool/RemoteFileManager.kt
@@ -1,10 +1,20 @@
 package com.example.myapplication.tool
 
+import android.os.Environment
+import android.util.Log
 import com.example.myapplication.entity.Files
+import okhttp3.Call
+import okhttp3.Callback
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import okhttp3.Response
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
 
 class RemoteFileManager {
+
+    val TAG = "RemoteFileManager"
 
     // 获取远程文件列表
     fun getFileList(): ArrayList<Files>? {
@@ -23,6 +33,49 @@ class RemoteFileManager {
             return FilesTools().toArrayList(responseBody)
         }
         return null
+    }
+
+    // 下载网络文件
+    fun downloadRemoteFile(filename: String) {
+        val client = OkHttpClient()
+        val request = Request.Builder().apply {
+            get()
+            url("http://192.168.2.3:8080/file/download/$filename")
+        }.build()
+
+        val call = client.newCall(request)
+        call.enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                Log.i(TAG, "onFailure: 下载失败")
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val file = File(
+                    Environment.getExternalStorageDirectory().absolutePath +
+                            "/Dav/$filename"
+                )
+                val inStream = response.body?.byteStream()
+                if (!file.exists()) {
+                    file.createNewFile()
+                } else {
+                    file.delete()
+                    file.createNewFile()
+                }
+                val outStream = FileOutputStream(file)
+                val buf = ByteArray(1024 * 1024)
+                var len = 0
+                while (true) {
+                    len = inStream!!.read(buf)
+                    if (len == -1) {
+                        break
+                    }
+                    outStream.write(buf, 0, len)
+                }
+                outStream.flush()
+                inStream.close()
+                outStream.close()
+            }
+        })
     }
 
 }
